eb3df1a5d973bac10caff0884faf0cbd7593d76f
David Brewster <david@myztery.cc>
2024-05-20 18:38:23 +1000

    Add missing vpc tf file

---
+++ vpc/main.tf
@@ -0,0 +1,11 @@
+# module.vpc.google_compute_network.network:
+resource "google_compute_network" "network" {
+    auto_create_subnetworks                   = true
+    delete_default_routes_on_create           = false
+    enable_ula_internal_ipv6                  = false
+    name                                      = var.vpc_name
+    project                                   = var.project_id
+    routing_mode                              = "GLOBAL"
+}   
+
+
