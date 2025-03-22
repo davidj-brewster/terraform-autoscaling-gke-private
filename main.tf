7203d5d2ced9fee9a6ea302cd0eea154f747d4c1
David Brewster <31854825+davidj-brewster@users.noreply.github.com>
2025-03-22 10:03:41 +0100

    GH workflow for tflint / terraform validate Updated google provider

--- main.tf
+++ main.tf
@@ -8,44 +8,44 @@ terraform {
   required_version = "1.5.7"
 }
 
-module "project" { 
-  source = "./project"
+module "project" {
+  source          = "./project"
   billing_account = var.billing_account
-  project_id = var.project_id
-  project_name = var.project_name
-  google_apis = var.google_apis
+  project_id      = var.project_id
+  project_name    = var.project_name
+  google_apis     = var.google_apis
 }
 
-module "vpc" { 
+module "vpc" {
   project_id = var.project_id
-  vpc_name = var.vpc_name
+  vpc_name   = var.vpc_name
   vpc_subnet = var.vpc_subnet
-  source = "./vpc"
-  depends_on = [ module.project ]
+  source     = "./vpc"
+  depends_on = [module.project]
 }
 
 module "firewall_rules" {
   source = "./firewall_rules"
-  depends_on = [ 
+  depends_on = [
     module.project,
     module.vpc
   ]
   project_id = var.project_id
-  vpc_name = var.vpc_name
+  vpc_name   = var.vpc_name
   vpc_subnet = var.vpc_subnet
 }
 
 module "gke" {
   source = "./gke"
-  depends_on = [ 
+  depends_on = [
     module.vpc,
     module.firewall_rules
   ]
-  project_id = var.project_id
-  billing_account = var.billing_account
-  region = var.region
-  vpc_name = var.vpc_name
-  vpc_subnet = var.vpc_subnet 
+  project_id         = var.project_id
+  billing_account    = var.billing_account
+  region             = var.region
+  vpc_name           = var.vpc_name
+  vpc_subnet         = var.vpc_subnet
   gke_serviceaccount = var.gke_serviceaccount
 }
 
--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "billing_account" {
-  type = string
+  type        = string
   description = "Required for project setup and some other operations"
 }
 
 variable "project_id" {
-  type = string
+  type        = string
   description = "The ID of the project that is created via terraform"
 }
 
 variable "project_name" {
-  type = string
+  type        = string
   description = "Project Name"
-  default = "GCP Terraform Setup Project"
+  default     = "GCP Terraform Setup Project"
 }
 
 variable "vpc_name" {
-  type = string
+  type        = string
   description = "Identifier of the VPC network"
-  default = "default-vpc"
+  default     = "default-vpc"
 }
 
 variable "google_apis" {
-  type = list(string)
+  type        = list(string)
   description = "Set of extra Google APIs that need to be enabled"
-  default = [ 
+  default = [
     "container.googleapis.com",
     "cloudbuild.googleapis.com",
     "run.googleapis.com",
@@ -36,20 +36,20 @@ variable "google_apis" {
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "The region in which to create the GKE cluster"
-  default = "asia-southeast1"
+  default     = "asia-southeast1"
 }
 
 variable "vpc_subnet" {
-  type = string
+  type        = string
   description = "Subnet name we'll use for GKE"
-  default = "default-vpc-subnet-gke-us"
+  default     = "default-vpc-subnet-gke-us"
 }
 
 variable "gke_serviceaccount" {
-  type = string
+  type        = string
   description = "Terraform managaed serviceaccount for the compute scope"
-  default = "svcacct-gke-compute-tfpoc-us"
+  default     = "svcacct-gke-compute-tfpoc-us"
 }
 
