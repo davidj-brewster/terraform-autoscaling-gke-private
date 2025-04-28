7e50ec7262ae31c7c9f6cc01f2cd8dc0eb174e17
David Brewster <31854825+davidj-brewster@users.noreply.github.com>
2025-04-28 18:59:26 +0200

    Update tf version to latest support by google provoder

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     google = {
-      source  = "google"
+      source  = "hashicorp/google"
       version = "~> 6.32.0"
     }
   }
-  required_version = "1.11.4"
+  required_version = ">= 1.5.7"
 }
 
 module "project" {
